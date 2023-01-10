52fad54bee35428b9e1315ce9d3352185e71c538
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-10 18:37:17 +0100

    - Design the question component - Decode the question with html-entities

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,13 @@
 import { QuestionType } from "../../interfaces/QuizType";
+import { decode } from "html-entities";
+import {
+  Container,
+  Main,
+  DifficultyQuestion,
+  NumberQuestion,
+  Answer,
+  QuestionStyled,
+} from "./styles";
 
 type QuestionProps = {
   question: QuestionType;
@@ -6,8 +15,20 @@ type QuestionProps = {
 };
 
 const Question = ({ question, currentQuestion }: QuestionProps) => {
-  console.log(question);
-  return <div>Question</div>;
+  const { difficulty, question: q, correct_answer } = question;
+  return (
+    <Main>
+      <Container>
+        <NumberQuestion>{currentQuestion}</NumberQuestion>
+        <h2>Question</h2>
+        <DifficultyQuestion difficulty={difficulty}>
+          {difficulty}
+        </DifficultyQuestion>
+      </Container>
+      <QuestionStyled>{decode(q)}</QuestionStyled>
+      <Answer>{correct_answer}</Answer>
+    </Main>
+  );
 };
 
 export default Question;
--- src/components/Question/styles.tsx
+++ src/components/Question/styles.tsx
@@ -1,1 +1,60 @@
 import styled from "styled-components";
+import { Difficulty } from "../../interfaces/QuizType";
+
+export const Main = styled.main`
+  border: 2.5px solid #13193e;
+  border-radius: 20px;
+  width: 30%;
+  padding: 2.5rem 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2.2rem;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NumberQuestion = styled.div`
+  background-color: #13193e;
+  color: #fff;
+  font-size: 20px;
+  padding: 0.2rem 0.5rem;
+  border-radius: 4px;
+`;
+
+export const DifficultyQuestion = styled.div<{ difficulty: Difficulty }>`
+  padding: 0.3rem 0.8rem;
+  border-radius: 4px;
+  color: #fff;
+  background-color: ${(props) =>
+    props.difficulty === "easy"
+      ? "#108407"
+      : props.difficulty === "medium"
+      ? "#ECA919"
+      : "#A41910"};
+`;
+
+export const QuestionStyled = styled.p`
+  font-size: 1.2rem;
+  font-weight: 800;
+`;
+
+export const Answer = styled.button`
+  width: 100%;
+  padding: 1rem;
+  border: none;
+  border-radius: 20px;
+  font-weight: 700;
+  color: #000;
+  background-color: #dcd8d1;
+  cursor: pointer;
+  :hover {
+    background-color: #bbbab8;
+  }
+`;
