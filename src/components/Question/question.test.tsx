192806f445744462069e85ea7c592cbccf6b1ad4
inmacc96 <inmacaballerocarrero@gmail.com>
2023-02-22 13:21:46 +0100

    Add tests to question component

--- src/components/Question/question.test.tsx
+++ src/components/Question/question.test.tsx
@@ -1,22 +1,22 @@
-import { describe, test, vi, expect } from 'vitest'
-import { prettyDOM, render, fireEvent } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import Question from '.'
-import { Difficulty } from '../../interfaces/QuizType'
-import { decode } from 'html-entities'
+import { describe, test, vi, expect } from "vitest";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Question from ".";
+import { Difficulty } from "../../interfaces/QuizType";
+import { decode } from "html-entities";
 
-describe('<Question />', () => {
-  test('renders correctly', () => {
-    const currentQuestion = 1
+describe("<Question />", () => {
+  test("renders correctly", () => {
+    const currentQuestion = 1;
     const question = {
       question:
-        'Which anime heavily features music from the genre &quot;Eurobeat&quot;?',
-      correct_answer: 'Initial D',
-      incorrect_answers: ['Wangan Midnight', 'Kino no Tabi', 'Cowboy Bebop'],
+        "Which anime heavily features music from the genre &quot;Eurobeat&quot;?",
+      correct_answer: "Initial D",
+      incorrect_answers: ["Wangan Midnight", "Kino no Tabi", "Cowboy Bebop"],
       difficulty: Difficulty.Easy,
-    }
-    const updateScore = vi.fn()
-    const nextQuestion = vi.fn()
+    };
+    const updateScore = vi.fn();
+    const nextQuestion = vi.fn();
 
     const component = render(
       <Question
@@ -25,27 +25,29 @@ describe('<Question />', () => {
         updateScore={updateScore}
         nextQuestion={nextQuestion}
       />
-    )
+    );
 
-    component.getByText('Question')
-    component.getByText(currentQuestion)
-    component.getByText(decode(question.question))
-    component.getByText(question.correct_answer)
-    component.getByText(question.incorrect_answers[0])
-    component.getByText(question.difficulty)
-  })
+    component.getByText("Question");
+    component.getByText(currentQuestion);
+    component.getByText(decode(question.question));
+    component.getByText(question.correct_answer);
+    component.getByText(question.incorrect_answers[0]);
+    component.getByText(question.difficulty);
+  });
 
-  test('clicking on the correct answer, updateScore is called once', () => {
-    const currentQuestion = 1
+  test("clicking on an answer, the button is disabled", async () => {
+    const user = userEvent.setup();
+
+    const currentQuestion = 1;
     const question = {
       question:
-        'Which anime heavily features music from the genre &quot;Eurobeat&quot;?',
-      correct_answer: 'Initial D',
-      incorrect_answers: ['Wangan Midnight', 'Kino no Tabi', 'Cowboy Bebop'],
+        "Which anime heavily features music from the genre &quot;Eurobeat&quot;?",
+      correct_answer: "Initial D",
+      incorrect_answers: ["Wangan Midnight", "Kino no Tabi", "Cowboy Bebop"],
       difficulty: Difficulty.Easy,
-    }
-    const updateScore = vi.fn()
-    const nextQuestion = vi.fn()
+    };
+    const updateScore = vi.fn();
+    const nextQuestion = vi.fn();
 
     const component = render(
       <Question
@@ -54,25 +56,30 @@ describe('<Question />', () => {
         updateScore={updateScore}
         nextQuestion={nextQuestion}
       />
-    )
+    );
 
-    const correctAnswerButton = component.getByText(question.correct_answer)
-    fireEvent.click(correctAnswerButton)
+    const answerButton = component.getByText(question.correct_answer);
+    expect(answerButton).not.toBeDisabled();
 
-    expect(updateScore).toHaveBeenCalledTimes(1)
-  })
+    await user.click(answerButton);
 
-  test('clicking on an answer, the next button appears', () => {
-    const currentQuestion = 1
+    const disabledAnswerButton = component.getByText(question.correct_answer);
+    expect(disabledAnswerButton).toBeDisabled();
+  });
+
+  test("clicking on the correct answer, updateScore is called once", async () => {
+    const user = userEvent.setup();
+
+    const currentQuestion = 1;
     const question = {
       question:
-        'Which anime heavily features music from the genre &quot;Eurobeat&quot;?',
-      correct_answer: 'Initial D',
-      incorrect_answers: ['Wangan Midnight', 'Kino no Tabi', 'Cowboy Bebop'],
+        "Which anime heavily features music from the genre &quot;Eurobeat&quot;?",
+      correct_answer: "Initial D",
+      incorrect_answers: ["Wangan Midnight", "Kino no Tabi", "Cowboy Bebop"],
       difficulty: Difficulty.Easy,
-    }
-    const updateScore = vi.fn()
-    const nextQuestion = vi.fn()
+    };
+    const updateScore = vi.fn();
+    const nextQuestion = vi.fn();
 
     const component = render(
       <Question
@@ -81,26 +88,27 @@ describe('<Question />', () => {
         updateScore={updateScore}
         nextQuestion={nextQuestion}
       />
-    )
+    );
 
-    const correctAnswerButton = component.getByText(question.correct_answer)
-    fireEvent.click(correctAnswerButton)
+    const correctAnswerButton = component.getByText(question.correct_answer);
+    await user.click(correctAnswerButton);
 
-    const btnNext = component.getByTestId('next-button')
-    expect(btnNext).toBeInTheDocument()
-  })
+    expect(updateScore).toHaveBeenCalledTimes(1);
+  });
 
-  test('clicking on next button, nextQuestion is called one', () => {
-    const currentQuestion = 1
+  test("clicking on a wrong answer, updateScore is not called", async () => {
+    const user = userEvent.setup();
+
+    const currentQuestion = 1;
     const question = {
       question:
-        'Which anime heavily features music from the genre &quot;Eurobeat&quot;?',
-      correct_answer: 'Initial D',
-      incorrect_answers: ['Wangan Midnight', 'Kino no Tabi', 'Cowboy Bebop'],
+        "Which anime heavily features music from the genre &quot;Eurobeat&quot;?",
+      correct_answer: "Initial D",
+      incorrect_answers: ["Wangan Midnight", "Kino no Tabi", "Cowboy Bebop"],
       difficulty: Difficulty.Easy,
-    }
-    const updateScore = vi.fn()
-    const nextQuestion = vi.fn()
+    };
+    const updateScore = vi.fn();
+    const nextQuestion = vi.fn();
 
     const component = render(
       <Question
@@ -109,14 +117,75 @@ describe('<Question />', () => {
         updateScore={updateScore}
         nextQuestion={nextQuestion}
       />
-    )
+    );
 
-    const AnswerButton = component.getByText(question.incorrect_answers[0])
-    fireEvent.click(AnswerButton)
+    const wrongAnswerButton = component.getByText(
+      question.incorrect_answers[0]
+    );
+    await user.click(wrongAnswerButton);
 
-    const btnNext = component.getByTestId('next-button')
-    fireEvent.click(btnNext)
+    expect(updateScore).toHaveBeenCalledTimes(0);
+  });
 
-    expect(nextQuestion).toBeCalledTimes(1)
-  })
-})
+  test("clicking on an answer, the next button appears", async () => {
+    const user = userEvent.setup();
+
+    const currentQuestion = 1;
+    const question = {
+      question:
+        "Which anime heavily features music from the genre &quot;Eurobeat&quot;?",
+      correct_answer: "Initial D",
+      incorrect_answers: ["Wangan Midnight", "Kino no Tabi", "Cowboy Bebop"],
+      difficulty: Difficulty.Easy,
+    };
+    const updateScore = vi.fn();
+    const nextQuestion = vi.fn();
+
+    const component = render(
+      <Question
+        question={question}
+        currentQuestion={currentQuestion}
+        updateScore={updateScore}
+        nextQuestion={nextQuestion}
+      />
+    );
+
+    const correctAnswerButton = component.getByText(question.correct_answer);
+    await user.click(correctAnswerButton);
+
+    const btnNext = component.getByTestId("next-button");
+    expect(btnNext).toBeInTheDocument();
+  });
+
+  test("clicking on next button, nextQuestion is called one", async () => {
+    const user = userEvent.setup();
+
+    const currentQuestion = 1;
+    const question = {
+      question:
+        "Which anime heavily features music from the genre &quot;Eurobeat&quot;?",
+      correct_answer: "Initial D",
+      incorrect_answers: ["Wangan Midnight", "Kino no Tabi", "Cowboy Bebop"],
+      difficulty: Difficulty.Easy,
+    };
+    const updateScore = vi.fn();
+    const nextQuestion = vi.fn();
+
+    const component = render(
+      <Question
+        question={question}
+        currentQuestion={currentQuestion}
+        updateScore={updateScore}
+        nextQuestion={nextQuestion}
+      />
+    );
+
+    const AnswerButton = component.getByText(question.incorrect_answers[0]);
+    await user.click(AnswerButton);
+
+    const btnNext = component.getByTestId("next-button");
+    await user.click(btnNext);
+
+    expect(nextQuestion).toBeCalledTimes(1);
+  });
+});
