7e9a18f49072ea06404a24bdf9f7f3c40120b6d3
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-02-28 17:08:43 +0100

    Improve styles

--- src/components/Layout/styles.tsx
+++ src/components/Layout/styles.tsx
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100vh;
+  min-height: 100vh;
   background-image: linear-gradient(
     to left top,
     #b14f1d,
